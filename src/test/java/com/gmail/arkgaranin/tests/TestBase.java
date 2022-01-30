f2d2200bcd88e3d32623058bd56f5f1ecfcaf0af
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-01-30 18:41:50 +0300

    Добавил класс TestBase

---
+++ src/test/java/com/gmail/arkgaranin/tests/TestBase.java
@@ -0,0 +1,14 @@
+package com.gmail.arkgaranin.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+  @BeforeAll
+  static void setup() {
+    Configuration.baseUrl = "https://demoqa.com";
+    Configuration.browserSize = "1920x1080";
+//    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+  }
+}
